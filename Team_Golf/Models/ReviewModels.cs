0b1ec9013a86620e735250033d70ad7f19d4e0ef
Daniel Roman <dan.roman21@gmail.com>
2020-06-17 11:30:46 -0400

    Added ReviewModels class and installed NuGet packages

---
+++ Team_Golf/Models/ReviewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_Golf.Models
+{
+    public class ReviewModels
+    {
+        public string Reviewer_Name { get; set; }
+        public int Review_Id { get; set; }
+        public string Review { get; set; }
+        public DateTime Review_Date { get; set; }
+        public int Review_Rating { get; set; }
+
+        public ReviewModels()
+        {
+
+        }
+        public ReviewModels(string reviewer_name, int review_id, string review, DateTime review_date, int review_rating)
+        {
+            Reviewer_Name = reviewer_name;
+            Review_Id = review_id;
+            Review = review;
+            Review_Date = review_date;
+            Review_Rating = review_rating;
+        }
+    }
+}
