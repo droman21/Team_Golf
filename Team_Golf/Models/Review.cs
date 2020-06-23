f02d2d27a3459eda88acc06cbb842b64bc7d3453
Chuck R <rapmastercm@aol.com>
2020-06-23 12:13:26 -0400

    addReviewForm

--- Team_Golf/Models/Review.cs
+++ Team_Golf/Models/Review.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Team_Golf.Models
 {
     public class Review
     {
+        [Required]
         public string ReviewerName { get; set; }
         public int Id { get; set; }
+        [Required]
         public string ReviewText { get; set; }
         public string ReviewDate { get; set; }
         public int ReviewRating { get; set; }
--- Team_Golf/Repositories/IRepository.cs
+++ Team_Golf/Repositories/IRepository.cs
@@ -9,5 +9,6 @@ namespace Team_Golf.Repositories
     {
         IEnumerable<T> GetAll();
         T GetById(int id);
+        void Create(T obj);
     }
 }
